9894815904acf27965f8e49b5b73d70545280374
Salih Yılmaz <salihyilmaz@kod.com.tr>
2019-05-17 11:21:03 +0300

    Redis Endpoint added

--- Redis/Controllers/RedisController.cs
+++ Redis/Controllers/RedisController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Redis.Models;
+using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,22 @@ namespace Redis.Controllers
         // GET: Redis
         public ActionResult Index()
         {
-            return View();
+            RedisEndpoint redisEndpoint = new RedisEndpoint("127.0.0.1",6379);
+            RedisClient client = new RedisClient(redisEndpoint);
+            UserModel userModel = new UserModel()
+            {
+                Username="Salih Can",
+                Email="aaa@aa.com",
+                Password="12345"
+            };
+            var serialized = JsonConvert.SerializeObject(userModel);
+            client.SetValue("UserInfo", serialized);
+            var getUserInfo =JsonConvert.DeserializeObject<UserModel>(client.GetValue("UserInfo"));
+            return View(getUserInfo);
         }
+
+
+
+
     }
 }
